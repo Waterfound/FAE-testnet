0e0461aed9a7b1c23abee19a1a44342bc67faedf
Waterfound <80145494+Waterfound@users.noreply.github.com>
2026-09-22 18:43:51 -0300

    ASIC F2 HLS: disable automatic loop pipelining for tractability

--- labs/asic-f2-pre-go/hls/run_hls.tcl
+++ labs/asic-f2-pre-go/hls/run_hls.tcl
@@ -6,6 +6,10 @@ open_solution -reset sol_vu47p -flow_target vivado
 set_part {xcvu47p-fsvh2892-2-e}
 create_clock -period 4.000 -name default
 config_interface -m_axi_addr64
+# Tractability-first synthesis: disable automatic loop pipelining/unrolling cascades.
+# AMD UG1399 documents pipeline_loops=0 as disabling automatic loop pipelining.
+config_compile -pipeline_loops 0
+config_compile -performance_budgeter disable
 csim_design
 csynth_design
 export_design -format ip_catalog
